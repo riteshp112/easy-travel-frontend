244e81ee3be625f596a6a9754ede4bb446c3ae65
Ritesh <ritesh.patel@daffodilsw.com>
2024-09-29 07:53:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {AppNavigator} from './routes/AppNavigator';
 import {BrowserRouter} from 'react-router-dom';
-
 import {ThemeProvider, createTheme} from '@mui/material';
-
 import {Theme} from '@radix-ui/themes';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import {ToastContainer} from 'react-toastify';
@@ -11,17 +9,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import theme from './theme/MaterialTheme';
 import {AiProvider} from './context/AiContext';
 
-
 function App() {
   return (
     <>
       <AiProvider>
+        <ThemeProvider theme={createTheme(theme)}>
+          <Theme>
             <GoogleOAuthProvider clientId="1088606169663-df95s64h0ihj8prru5mcf2e3hm2ehe3o.apps.googleusercontent.com">
               <BrowserRouter basename="/">
                 <AppNavigator />
                 <ToastContainer />
               </BrowserRouter>
             </GoogleOAuthProvider>
+          </Theme>
+        </ThemeProvider>
       </AiProvider>
     </>
   );
